0576c3706ae1334c8177edd2dd083add84fe909e
tomsound <jirapong1984@gmail.com>
2026-08-26 16:45:15 +0700

    Increase body parser payload limit to 50mb

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const path = require('path');
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
-
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 let db;
 
 (async () => {
